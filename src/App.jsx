4b9328341a1d9e1dfec0224819711ff2d18a7b47
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-03-02 16:55:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Carrousel from './Components/Carrucel/Carrousel'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Carrousel/>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../NavBar/NavBar.css"
+
+function NavBar() {
+  return (
+      <div className='Conteiner-Nav'>
+          <a className='Link-Nav' href="">HOME</a>
+          <a className='Link-Nav' href="">CONTACTS</a>
+      <a className='Link-Nav' href="">INFO</a>
+      <a></a>
+      </div>
+  )
+}
+
+export default NavBar
