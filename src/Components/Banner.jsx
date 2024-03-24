0bd770dc9a9cc01b05fcdb579bc11b0e832f6d8d
ClaudioBarlassina <claudio_barlassina_ar@hotmail.com>
2024-03-24 13:38:23 -0300

    efectos

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 function Banner() {
   const [loopNum, setLoopNum] = useState(0);
@@ -25,7 +27,7 @@ function Banner() {
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-   
+
     let updatedText = isDeleting
       ? fullText.substring(0, text.length - 1)
       : fullText.substring(0, text.length + 1);
@@ -48,22 +50,32 @@ function Banner() {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline "> Welcome to Portfolio</span>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animated__animated animate__slideInUp" : ""
+                  }
+                >
+                  <span className="tagline "> Welcome to Portfolio</span>
 
-            <h1>
-              {"Hi I'm WebDecode "}
-              <span className="wrap">{text}</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-              perspiciatis distinctio atque necessitatibus minus sapiente minima
-              aperiam, quia eos, sed, nesciunt dolorem aspernatur maiores vero
-              exercitationem repudiandae nihil quidem velit!
-            </p>
-            <button onClick={() => console.log("connect")}>
-              {" "}
-              Let's connect <ArrowRightCircle size={25} />
-            </button>
+                  <h1>
+                    {"Hi I'm WebDecode "}
+                    <span className="wrap">{text}</span>
+                  </h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                    perspiciatis distinctio atque necessitatibus minus sapiente
+                    minima aperiam, quia eos, sed, nesciunt dolorem aspernatur
+                    maiores vero exercitationem repudiandae nihil quidem velit!
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    {" "}
+                    Let's connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <img src={headerImg} alt="headder Img"></img>
